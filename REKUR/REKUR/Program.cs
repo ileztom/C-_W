371fb4b885ca9d80e109b222bc28e2e4ef084c97
ileztom <91593668+ileztom@users.noreply.github.com>
2024-02-07 12:05:12 +0300

    Add files via upload

---
+++ REKUR/REKUR/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace REKUR
+{
+    internal class Program
+    {
+        static int CalculateF(int n)
+        {
+            if (n <= 2)
+            {
+                return n + 3;
+            }
+            else
+            {
+                return CalculateF(n - 1) + CalculateF(n - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int result = CalculateF(a);
+            Console.WriteLine($"F({a}) = {result}");
+        }
+    }
+}
