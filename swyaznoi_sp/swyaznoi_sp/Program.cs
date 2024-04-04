29ea7b97935ac16e79b3dff96e1ff51ab1022f07
ileztom <91593668+ileztom@users.noreply.github.com>
2024-04-05 00:46:30 +0300

    Add files via upload

---
+++ swyaznoi_sp/swyaznoi_sp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace swyaznoi_sp
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode FindMiddleNode(ListNode head)
+        {
+            if (head == null) return null;
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode node5 = new ListNode(5);
+            ListNode node4 = new ListNode(4, node5);
+            ListNode node3 = new ListNode(3, node4);
+            ListNode node2 = new ListNode(2, node3);
+            ListNode node1 = new ListNode(1, node2);
+
+            Solution solution = new Solution();
+            ListNode middleNode = solution.FindMiddleNode(node1);
+            Console.WriteLine("Середина связного списка " + middleNode.val);
+        }
+    }
+}
